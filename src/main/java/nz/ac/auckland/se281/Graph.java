3237b34486afda65ee1ffb15726e77ef225206ce
Ajith <ajith.penmatsa001@gmail.com>
2024-05-25 20:19:37 +1200

    created a method in graph class to create an edge between 2 adjacent countries

---
+++ src/main/java/nz/ac/auckland/se281/Graph.java
@@ -0,0 +1,23 @@
+package nz.ac.auckland.se281;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Graph {
+  private Map<String, List<String>> adjNodes;
+
+  public Graph() {
+    this.adjNodes = new HashMap<>();
+  }
+
+  public void addNode(String node) {
+    adjNodes.putIfAbsent(node, new ArrayList<>());
+  }
+
+  public void addEdge(String node1, String node2) {
+    adjNodes.get(node1).add(node2);
+    adjNodes.get(node2).add(node1);
+  }
+}
